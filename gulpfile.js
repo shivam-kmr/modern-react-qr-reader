ffc3ad68d8d65fa5de71ca94d97a5f39b9152cda
Shivam Kumar <shivam34.kumar@ril.com>
2025-03-02 01:47:06 +0530

    Upgrade Gulp to version 5 and refactor gulpfile for improved task definitions

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,53 @@
-const gulp = require('gulp')
-const fs = require('fs')
-const del = require('del')
-const inlineStr = require('gulp-inline-str')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
+const { src, dest, series, parallel, watch } = require("gulp");
+const fs = require("fs");
+const del = require("del");
+const inlineStr = require("gulp-inline-str");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
 
-const babelOptions = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'))
+const babelOptions = JSON.parse(fs.readFileSync("./.babelrc", "utf8"));
 
 const paths = {
-  scripts: [ 'src/index.js', 'src/getDeviceId.js', 'src/havePropsChanged.js', 'src/errors.js', 'src/createBlob.js' ],
-  worker: 'src/worker.js',
-  jsQR: 'node_modules/jsqr/dist/jsQR.js',
-  destination: './lib',
+  scripts: ["src/index.js", "src/getDeviceId.js", "src/havePropsChanged.js", "src/errors.js", "src/createBlob.js"],
+  worker: "src/worker.js",
+  jsQR: "node_modules/jsqr/dist/jsQR.js",
+  destination: "./lib",
+};
+
+// Clean task
+function clean() {
+  return del([`${paths.destination}/*.js`]);
 }
 
-gulp.task('clean', function() {
-  return del([ paths.destination + '/*.js' ])
-})
-
-gulp.task('worker', [ 'clean' ], function() {
-  return gulp
-    .src([ paths.jsQR, paths.worker ])
-    .pipe(concat('worker.js'))
+// Worker task
+function worker() {
+  return src([paths.jsQR, paths.worker])
+    .pipe(concat("worker.js"))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.destination))
-})
+    .pipe(dest(paths.destination));
+}
 
-gulp.task('build', [ 'worker' ], function() {
-  return gulp
-    .src(paths.scripts)
+// Build task
+function build() {
+  return src(paths.scripts)
     .pipe(inlineStr({ basePath: paths.destination }))
     .pipe(babel(babelOptions))
-    .pipe(gulp.dest(paths.destination))
-})
+    .pipe(dest(paths.destination));
+}
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-  gulp.watch(paths.scripts, [ 'build' ])
-})
+// Watch task
+function watchFiles() {
+  watch(paths.scripts, build);
+}
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', [ 'build' ])
+// Define complex tasks
+const buildTask = series(clean, worker, build);
+const defaultTask = buildTask;
+
+// Export tasks
+exports.clean = clean;
+exports.worker = worker;
+exports.build = buildTask;
+exports.watch = watchFiles;
+exports.default = defaultTask;
